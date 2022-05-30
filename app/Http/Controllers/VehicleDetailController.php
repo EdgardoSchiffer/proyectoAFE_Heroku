dc12c8caf69844e08fd0f08d63edaca735da2e8a
nic <nicolasesc@outlook.com>
2022-05-30 09:12:54 -0600

    Add model, controler, migration, seeder all class

---
+++ app/Http/Controllers/VehicleDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vehicle_detail;
+use Illuminate\Http\Request;
+
+class VehicleDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vehicle_detail  $vehicle_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vehicle_detail $vehicle_detail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vehicle_detail  $vehicle_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vehicle_detail $vehicle_detail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Vehicle_detail  $vehicle_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vehicle_detail $vehicle_detail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vehicle_detail  $vehicle_detail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vehicle_detail $vehicle_detail)
+    {
+        //
+    }
+}
