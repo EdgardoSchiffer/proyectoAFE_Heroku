989896d00278452d89183aa07ff0115d9cf251bc
nic <nicolasesc@outlook.com>
2022-05-30 14:22:54 -0600

    new data in rentals table

--- database/migrations/2022_05_30_150907_create_rentals_table.php
+++ database/migrations/2022_05_30_150907_create_rentals_table.php
@@ -15,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('rentals', function (Blueprint $table) {
             $table->id();
+            $table->timestamps('rental_date'); //fecha que se realiza el alquiler
+            $table->timestamps('rental_withdrawal_date'); //fecha de retiro del vehiculo por parte del cliente
+            $table->timestamps('delivery_date'); //fecha de entrega del vehiculo al cliente
+            $table->foreignId('vehicle_id')->reference('id')->on('vehicles'); 
+            $table->foreignId('client_id')->reference('id')->on('clients'); //usuario que hace la reserva
+            $table->float('advance'); 
+            $table->float('late_delivery_charge');//retorno del vehiculo por parte del cliente
+            $table->string('comment');
+            $table->float('damage_charge');
+            $table->integer('rental_time'); //numero de dias que se renta el vehiculo partiendo de la fecha de entrega(delivery_date)
+            $table->integer('user_delivery_id')->nullable();
+            $table->integer('user_receive_id')->nullable();
             $table->timestamps();
         });
     }
