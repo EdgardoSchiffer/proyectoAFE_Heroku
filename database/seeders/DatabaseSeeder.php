bd3b51559c9effa11668dbc25491f12df60cb52f
bryangmz21 <bryangmz21@hotmail.com>
2022-05-30 15:51:35 -0600

    Migrations & seeder for vehicles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,5 +31,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AccessoryTypeSeeder::class);
         $this->call(AccessorySeeder::class);
         $this->call(VehicleTypeSeeder::class);
+        $this->call(VehicleSeeder::class);
     }
 }
