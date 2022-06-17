ee59dbd46a81eec43e33af53fdfad47afb6e5166
nic <nicolasesc@outlook.com>
2022-06-17 14:01:33 -0600

    reload images

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -13,9 +13,10 @@ class CommentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index() 
     {
         //
+        
     }
 
     /**
